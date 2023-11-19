bbdb15c45fe37a1dffff16ef627fcd8c706538d2
isjsm <84210598+isjsm@users.noreply.github.com>
2023-11-19 17:38:10 +0300

    Update Anoymous.sh

--- Anoymous.sh
+++ Anoymous.sh
@@ -4,6 +4,9 @@ figlet Anonymous
 echo"The Tool Made By : https://t.me/Termux_Commamd"
 echo"Talk To Dev Tool In Telegram : @TalkToDevTermuxBot"
 
-echo "Type "AMcmd" To Show Tools"
+echo "Type "Hackers" To Show Tools"
 
-
+read -p "Terminal_$ " tar
+if [ $tar == "Hackers" ]
+then
+echo "① Type "D" To Open Tool ddos atack"
