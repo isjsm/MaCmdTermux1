6d8c856eacd5bdcb1b73e035b4ebbb99e42afa9b
isjsm <84210598+isjsm@users.noreply.github.com>
2023-11-19 18:00:51 +0300

    Update Anoymous.sh

--- Anoymous.sh
+++ Anoymous.sh
@@ -1,8 +1,8 @@
 apt-get install figlet
 clear
 figlet Tools680
-echo"The Tool Made By : https://t.me/Termux_Commamd"
-echo"Talk To Dev Tool In Telegram : @TalkToDevTermuxBot"
+echo" The Tool Made By : https://t.me/Termux_Commamd "
+echo" Talk To Dev Tool In Telegram : @TalkToDevTermuxBot "
 
 echo "Type «tools» To Show Tools"
 read -p "Terminal_$ " tar
