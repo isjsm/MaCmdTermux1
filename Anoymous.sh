b15ed0479514b1f70f491ee92df2d7beec9374c0
isjsm <84210598+isjsm@users.noreply.github.com>
2023-11-20 18:44:00 +0300

    Update Anoymous.sh

--- Anoymous.sh
+++ Anoymous.sh
@@ -1,13 +1,13 @@
 apt-get install figlet
 clear
 figlet Tools680
-
-echo "The Tool Made By : https://t.me/Termux_Commamd"
-
-echo "Talk To Dev Tool In Telegram : @TalkToDevTermuxBot"
-
-echo "Type 'tools' To Show Tools"
+echo ``
+echo `The Tool Made By : https://t.me/Termux_Commamd`
+echo ``
+echo `Talk To Dev Tool In Telegram : @TalkToDevTermuxBot`
+echo ``
+echo Type 'tools' To Show Tools`
 read -p "Terminal_$ " tar
 if [ "$tar" == "tools" ]
 then
-  echo "① Type '1' To Open DDOS ATACK Tool"
+  echo `① Type '1' To Open DDOS ATACK Tool`
