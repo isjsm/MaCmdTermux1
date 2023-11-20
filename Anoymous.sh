55a50caf62bbcb6069ef49a6c38a3712b50251cd
isjsm <84210598+isjsm@users.noreply.github.com>
2023-11-20 18:41:52 +0300

    Update Anoymous.sh

--- Anoymous.sh
+++ Anoymous.sh
@@ -1,1 +1,13 @@
+apt-get install figlet
+clear
+figlet Tools680
 
+echo "The Tool Made By : https://t.me/Termux_Commamd"
+
+echo "Talk To Dev Tool In Telegram : @TalkToDevTermuxBot"
+
+echo "Type 'tools' To Show Tools"
+read -p "Terminal_$ " tar
+if [ "$tar" == "tools" ]
+then
+  echo "① Type '1' To Open DDOS ATACK Tool"
